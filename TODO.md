575ae71d6c33f64f04ee46ff958ef92dc312314e
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-09-11 15:05:43 -0700

    Made it work with literals like ? and *.

--- TODO.md
+++ TODO.md
@@ -2,6 +2,5 @@
 
 # TODO
 
-* Make sure literals work [?]
 * Make it work on Windows
 * Make it work with ranges
--- test/main.d
+++ test/main.d
@@ -7,7 +7,12 @@ unittest {
 	describe("glob",
 		it("Should work with relative paths", delegate() {
 			glob("test/test_data/*").shouldEqual([
-				"test/test_data/aaa", "test/test_data/bbb", "test/test_data/ccc"]);
+				"test/test_data/aaa", "test/test_data/bbb", "test/test_data/ccc",
+				"test/test_data/test*", "test/test_data/test1", "test/test_data/test?"]);
+		}),
+		it("Should work with literals", delegate() {
+			glob("test/test_data/test[?]").shouldEqual(["test/test_data/test?"]);
+			glob("test/test_data/test[*]").shouldEqual(["test/test_data/test*"]);
 		}),
 		it("Should glob nothing on invalid path", delegate() {
 			glob("/does/not/exist").shouldEqual([]);
