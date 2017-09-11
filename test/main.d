cd356cc38ef10dfb7e04ccd8c2c5bdf8e32d7b16
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-09-11 13:31:53 -0700

    Added more tests.

--- test/main.d
+++ test/main.d
@@ -24,7 +24,11 @@ unittest {
 				"/usr/bin/python3.5m", "/usr/bin/python2", "/usr/bin/python2-config",
 				"/usr/bin/python-config", "/usr/bin/python2-jsonschema", "/usr/bin/python2-pbr"]);
 		}),
-		it("Should glob dir name single ?", delegate() {
+		it("Should glob base name single ?", delegate() {
+			glob("/usr/bin/python?").shouldEqual([
+				"/usr/bin/python3", "/usr/bin/python2"]);
+		}),
+		it("Should glob multiple *", delegate() {
 			glob("/usr/*/python*").shouldEqual([
 				"/usr/share/python3", "/usr/share/python3-plainbox", "/usr/share/python",
 				"/usr/share/python-apt", "/usr/include/python2.7", "/usr/include/python3.5m",
@@ -34,6 +38,10 @@ unittest {
 				"/usr/bin/python-config", "/usr/bin/python2-jsonschema",
 				"/usr/bin/python2-pbr", "/usr/lib/python3", "/usr/lib/python2.7",
 				"/usr/lib/python3.5"]);
+		}),
+		it("Should glob * and ?", delegate() {
+			glob("/usr/*/python?").shouldEqual([
+				"/usr/share/python3", "/usr/bin/python3", "/usr/bin/python2", "/usr/lib/python3"]);
 		}),
 	);
 }
