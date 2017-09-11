4180c2c1f5b7e1dc02de73df97319084b5556850
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-09-11 08:50:01 -0700

    Added globbing of exact paths.

--- source/glob.d
+++ source/glob.d
@@ -12,9 +12,26 @@ import std.stdio : stdout;
 
 
 string[] glob(string path_name) {
+	import std.file : exists, isDir;
+	import std.path : buildPath, dirName, baseName;
 	string[] retvals;
+
+	// Break the path into base name and dir name
+	string dir_name = dirName(path_name);
+	string base_name = baseName(path_name);
+
+	// The path exists and has no wild cards
+	if (! hasWildCard(path_name) && exists(path_name)) {
+		retvals ~= path_name;
+		return retvals;
+	}
 
 	return retvals;
 }
 
+private bool hasWildCard(string s) {
+	import std.algorithm : canFind;
+	// FIXME: There has to be a simple way to do this
+	return s.canFind("*") || s.canFind("?") || s.canFind("[");
+}
 
--- test/main.d
+++ test/main.d
@@ -5,10 +5,13 @@ import std.stdio : stdout, stderr;
 import std.datetime : Date;
 
 unittest {
-
+	import glob : glob;
 	describe("glob",
-		it("Should glob multiple *", delegate() {
-
+		it("Should glob nothing on invalid path", delegate() {
+			glob("/does/not/exist").shouldEqual([]);
+		}),
+		it("Should glob exact path", delegate() {
+			glob("/usr/bin/python").shouldEqual(["/usr/bin/python"]);
 		}),
 	);
 }
