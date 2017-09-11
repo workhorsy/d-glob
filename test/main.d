ca18963a1aac6528c549bf8d2f077e5182fc3656
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-09-11 12:37:19 -0700

    Made existing tests pass.

--- test/main.d
+++ test/main.d
@@ -7,21 +7,23 @@ import std.datetime : Date;
 unittest {
 	import glob : glob3;
 	describe("glob",
-/*
 		it("Should glob nothing on invalid path", delegate() {
-			glob("/does/not/exist").shouldEqual([]);
+			glob3("/does/not/exist").shouldEqual([]);
 		}),
 		it("Should glob exact path", delegate() {
-			glob("/usr/bin/python").shouldEqual(["/usr/bin/python"]);
+			glob3("/usr/bin/python").shouldEqual(["/usr/bin/python"]);
 		}),
 
 		it("Should glob base name single ?", delegate() {
-			glob("/usr/bin/python?").shouldEqual(["/usr/bin/python3", "/usr/bin/python2"]);
+			glob3("/usr/bin/python?").shouldEqual(["/usr/bin/python3", "/usr/bin/python2"]);
 		}),
 		it("Should glob base name single *", delegate() {
-			glob("/usr/bin/python*").shouldEqual(["/usr/bin/python3", "/usr/bin/python2.7-config", "/usr/bin/python3m", "/usr/bin/python2.7", "/usr/bin/python", "/usr/bin/python3.5", "/usr/bin/python3.5m", "/usr/bin/python2", "/usr/bin/python2-config", "/usr/bin/python-config", "/usr/bin/python2-jsonschema", "/usr/bin/python2-pbr"]);
+			glob3("/usr/bin/python*").shouldEqual([
+				"/usr/bin/python3", "/usr/bin/python2.7-config", "/usr/bin/python3m",
+				"/usr/bin/python2.7", "/usr/bin/python", "/usr/bin/python3.5",
+				"/usr/bin/python3.5m", "/usr/bin/python2", "/usr/bin/python2-config",
+				"/usr/bin/python-config", "/usr/bin/python2-jsonschema", "/usr/bin/python2-pbr"]);
 		}),
-*/
 		it("Should glob dir name single ?", delegate() {
 			glob3("/usr/*/python*").shouldEqual([
 				"/usr/share/python3", "/usr/share/python3-plainbox", "/usr/share/python",
