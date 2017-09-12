7cf68d6ae9ae6021abd9bd89a15e67d3806b35e0
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-09-12 09:14:22 -0700

    Added an example

--- examples/example.d
+++ examples/example.d
@@ -2,8 +2,20 @@
 
 
 int main() {
-	import std.stdio : stdout, stderr;
+	import std.stdio : stdout;
+	import glob : glob;
 
+	// Print all the locations of gcc
+	stdout.writefln("Searching for gcc ...");
+	foreach (entry ; glob("/usr/{lib,bin}/gcc*")) {
+		stdout.writefln("    %s", entry);
+	}
+
+	// Print all the locations of python
+	stdout.writefln("Searching for python ...");
+	foreach (entry ; glob("/usr/*/python?")) {
+		stdout.writefln("    %s", entry);
+	}
 
 	return 0;
 }
