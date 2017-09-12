88c8bd93a5246a9bd55f85656df0610a412a66fa
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-09-12 09:45:14 -0700

    Added unit tests for matching {} and [].

--- TODO.md
+++ TODO.md
@@ -3,5 +3,4 @@
 # TODO
 
 * Make it work with ranges
-* Add other glob patterns to tests such as [] and {}
-https://dlang.org/phobos/std_path.html#globMatch
+
--- test/main.d
+++ test/main.d
@@ -6,42 +6,45 @@ unittest {
 	import glob : glob;
 
 	describe("glob",
-		it("Should work with relative paths", delegate() {
-			glob("test/test_data/*").shouldEqual([
-				"test/test_data/aaa", "test/test_data/bbb", "test/test_data/ccc",
-				"test/test_data/test1"]);
-		}),
 		it("Should work with empty paths", delegate() {
 			glob("/").shouldEqual(["/"]);
 			glob("").shouldEqual([]);
 			glob(" ").shouldEqual([]);
 		}),
-		it("Should glob nothing on invalid path", delegate() {
+		it("Should return nothing on invalid path", delegate() {
 			glob("/does/not/exist").shouldEqual([]);
 			glob("does/not/exist").shouldEqual([]);
 		}),
 		it("Should glob exact path", delegate() {
 			glob("test/test_data/bbb").shouldEqual(["test/test_data/bbb"]);
 		}),
-
-		it("Should glob base name single ?", delegate() {
+		it("Should work with relative paths", delegate() {
+			glob("test/test_data/*").shouldEqual([
+				"test/test_data/aaa", "test/test_data/bbb",
+				"test/test_data/ccc", "test/test_data/test1"]);
+		}),
+		it("Should glob ?", delegate() {
 			glob("test/test_data/aaa/example?").shouldEqual([
+				"test/test_data/aaa/example2", "test/test_data/aaa/example4",
+				"test/test_data/aaa/example9"]);
+		}),
+		it("Should glob *", delegate() {
+			glob("test/test_data/aaa/example*").shouldEqual([
+				"test/test_data/aaa/example", "test/test_data/aaa/example2",
+				"test/test_data/aaa/example4", "test/test_data/aaa/example9"]);
+		}),
+		it("Should glob {}", delegate() {
+			glob("test/test_data/{aaa,ccc}/").shouldEqual([
+				"test/test_data/aaa", "test/test_data/ccc"]);
+		}),
+		it("Should glob []", delegate() {
+			glob("test/test_data/aaa/example[29]").shouldEqual([
 				"test/test_data/aaa/example2", "test/test_data/aaa/example9"]);
 		}),
-		it("Should glob base name single *", delegate() {
-			glob("test/test_data/aaa/example*").shouldEqual([
-				"test/test_data/aaa/example", "test/test_data/aaa/example2", "test/test_data/aaa/example9"]);
-		}),
-		it("Should glob multiple *", delegate() {
-			glob("test/test_data/*/*").shouldEqual([
-				"test/test_data/aaa/example", "test/test_data/aaa/example2",
-				"test/test_data/aaa/example9", "test/test_data/bbb/example3",
-				"test/test_data/ccc/example4"]);
-		}),
-		it("Should glob * and ?", delegate() {
-			glob("test/test_data/*/example?").shouldEqual([
+		it("Should glob multiple", delegate() {
+			glob("test/*/{aaa,bbb}/example[29]").shouldEqual([
 				"test/test_data/aaa/example2", "test/test_data/aaa/example9",
-				"test/test_data/bbb/example3", "test/test_data/ccc/example4"]);
+				"test/test_data/bbb/example2", "test/test_data/bbb/example9"]);
 		}),
 	);
 }
