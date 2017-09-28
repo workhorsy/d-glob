570bd314c33c4f0a837ed86ff46d360820aca4ed
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-09-27 22:51:23 -0700

    Moved core glob logic to one function.

--- source/glob.d
+++ source/glob.d
@@ -26,53 +26,12 @@ import std.stdio : stdout;
 import std.regex : regex, Regex;
 
 /++
-Return all the paths that match the pattern
+Return all the paths that match the glob pattern
 Params:
  path_name = The path with paterns to match.
 +/
-string[] glob(string path_name, bool is_regex=false) {
-	import std.algorithm : map, filter;
-	import std.array : array, replace;
-	import std.string : split, startsWith;
-	import std.file : getcwd;
-
-	// Convert the Windows path to a posix path
-	path_name = path_name.replace("\\", "/");
-
-	// Break the path into a stack separated by /
-	string[] patterns = path_name.split("/").filter!(n => n != "").array();
-
-	// Figure out if using a relative path
-	string cwd = getcwd();
-	bool is_relative_path = ! path_name.startsWith("/");
-
-	// Make the first path to search the cwd or /
-	string[] paths = [is_relative_path ? cwd : "/"];
-//	stdout.writefln("path_name: \"%s\"", path_name);
-//	stdout.writefln("patterns: %s", patterns);
-
-	// For each pattern get the directory entries that match the pattern
-	while (patterns.length > 0) {
-		// Pop the next pattern off the stack
-		string pattern = patterns[0];
-		patterns = patterns[1 .. $];
-
-		// Get the matches
-		paths = getMatches(paths, pattern, is_regex);
-//		stdout.writefln("            paths: %s", paths);
-	}
-
-	// Convert from an absolute path to a relative one, if path_name is relative
-	if (is_relative_path) {
-		size_t len = cwd.length + 1;
-		paths =
-			paths
-				.filter!(n => n.length > len) // Remove paths that are just the prefix
-				.map!(n => n[len .. $]) // Remove the path prefix
-				.array();
-	}
-
-	return paths;
+string[] glob(string path_name) {
+	return getGlobMatches(path_name, false);
 }
 
 ///
@@ -122,6 +81,11 @@ unittest {
 	*/
 }
 
+/++
+Return all the paths that match the regex pattern
+Params:
+ path_regex = The path with regex paterns to match.
++/
 string[] globRegex(string path_regex) {
 	import std.string : startsWith, endsWith;
 
@@ -132,7 +96,68 @@ string[] globRegex(string path_regex) {
 	// Remove the regex ^ and $ from the start and end
 	string path_name = path_regex[1 .. $-1];
 
-	return glob(path_name, true);
+	return getGlobMatches(path_name, true);
+}
+
+///
+unittest {
+	import std.stdio : stdout;
+	import glob : globRegex;
+
+	// Use a regex to match all the number files in /proc/
+	string[] entries = globRegex(`^/proc/[0-9]*$`);
+	/*
+	entries would contain:
+	/proc/111
+	/proc/245
+	/proc/19533
+	/proc/1
+	*/
+}
+
+private string[] getGlobMatches(string path_name, bool is_regex) {
+	import std.algorithm : map, filter;
+	import std.array : array, replace;
+	import std.string : split, startsWith;
+	import std.file : getcwd;
+
+	// Convert the Windows path to a posix path
+	path_name = path_name.replace("\\", "/");
+
+	// Break the path into a stack separated by /
+	string[] patterns = path_name.split("/").filter!(n => n != "").array();
+
+	// Figure out if using a relative path
+	string cwd = getcwd();
+	bool is_relative_path = ! path_name.startsWith("/");
+
+	// Make the first path to search the cwd or /
+	string[] paths = [is_relative_path ? cwd : "/"];
+//	stdout.writefln("path_name: \"%s\"", path_name);
+//	stdout.writefln("patterns: %s", patterns);
+
+	// For each pattern get the directory entries that match the pattern
+	while (patterns.length > 0) {
+		// Pop the next pattern off the stack
+		string pattern = patterns[0];
+		patterns = patterns[1 .. $];
+
+		// Get the matches
+		paths = getMatches(paths, pattern, is_regex);
+//		stdout.writefln("            paths: %s", paths);
+	}
+
+	// Convert from an absolute path to a relative one, if path_name is relative
+	if (is_relative_path) {
+		size_t len = cwd.length + 1;
+		paths =
+			paths
+				.filter!(n => n.length > len) // Remove paths that are just the prefix
+				.map!(n => n[len .. $]) // Remove the path prefix
+				.array();
+	}
+
+	return paths;
 }
 
 private string[] getMatches(string[] path_candidates, string pattern, bool is_regex=false) {
